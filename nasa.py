1661d6118c2ab8bdf0951df4c0f55e26743e85c2
Sumit <67678753+Sumit-buddy@users.noreply.github.com>
2020-08-21 20:30:53 +0530

    Add files via upload

---
+++ nasa.py
@@ -0,0 +1,88 @@
+import time
+from cfonts import render
+import sys
+import emoji
+import os 
+import itertools
+import threading
+
+#my banner artwork **sumit**
+banner = render("nasa", colors=['blue', 'green'], align='center')
+print(banner)
+
+print(emoji.emojize(":Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner::Japanese_symbol_for_beginner:"))
+
+print("\n")
+
+print(emoji.emojize(":fire::fire::fire::fire::fire::fire:Author : Sumit Patidar:fire::fire::fire::fire::fire::fire:"))
+
+print("\n")
+
+print(emoji.emojize(":squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue::squinting_face_with_tongue:"))
+
+print("\n")
+
+# warnings go here
+print("\033[1;36mFor Educational Purpose Only ! Don't Misuse ! \033[00m \n")
+
+
+ask = input("\033[1;33;40mWarning! It Is Very Risky. Are You Serious ? \n\nHit Enter If You Are Ready To Take The Risk ==>\033[0m \n" )
+
+animation = ["[Processing your request | ]","[Processing your request / ]", "[Processing your request - ]", "[Processing your request \ ]"]
+
+for i in range(25):
+    time.sleep(0.1)
+    sys.stdout.write("\r" + animation[i % len(animation)])
+    sys.stdout.flush()
+
+print ("\n")
+#checking vulnerabilies <success>
+#resolving host.........<success>
+#preparing bruteforce...<success>
+#attacking local ftp....<success>
+#testing network flow...<success>
+#sending payload........<success>
+#fetching chache data...<success>
+#injecting malware......<success>
+#system overtaken.......<success>
+#fetching data..........<success>
+print("\033[1;36mAttacking NASA Please Wait ! \033[00m")
+print("\n")
+animation = ["checking vulnerabilies <success>■□□□□□□□□□]","[resolving host.........<success>■■□□□□□□□□]", "[preparing bruteforce...<success>■■■□□□□□□□]", "[attacking local ftp....<success>■■■■□□□□□□]", "[testing network flow...<success>■■■■■□□□□□]", "[sending payload........<success>■■■■■■□□□□]", "[fetching chache data...<success>■■■■■■■□□□]", "[injecting malware......<success>■■■■■■■■□□]", "[system overtaken.......<success>■■■■■■■■■□]", "[fetching data..........<success>■■■■■■■■■■]"]
+
+for i in range(len(animation)):
+    time.sleep(0.5)
+    sys.stdout.write("\r" + animation[i % len(animation)])
+    sys.stdout.flush()
+
+
+#server controllers......<HACKED>
+#administration pass....<FETCHED>
+#firewall security......<CROSSED>
+#control over ftp.........<DONE!>
+#MITM over system........<PASSED>
+#main database...........<HACKED>
+#all secret files........<STOLEN>
+#admin panel info........<HACKED>
+#system database.........<HACKED>
+#system overtaken..........<DONE>
+print ("\n")
+print ("\033[1;36;40mFinalising \033[0m")
+animation_1 = ["server controllers......<HACKED>[•+++++++++]","administration pass....<FETCHED>[••++++++++]", "firewall security......<CROSSED>[•••+++++++]", "control over ftp.........<DONE!>[••••++++++]", "MITM over system........<PASSED>[•••••+++++]", "main database...........<HACKED>[••••••++++]", "all secret files........<STOLEN>[•••••••+++]", "admin panel info........<HACKED>[••••••••++]", "system database.........<HACKED>[•••••••••+]", "system overtaken..........<DONE>[••••••••••]"]
+
+for j in range(len(animation_1)):
+    time.sleep(0.3)
+    sys.stdout.write("\r" + animation_1[j % len(animation_1)])
+    sys.stdout.flush()
+    
+    
+banner = render("done!", colors=['gray', 'green'], align='center')
+print(banner)
+
+  
+ 
+	
+
+
+        
+
